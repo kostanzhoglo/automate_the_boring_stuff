9b269c096670b6ffe70be7e4d68643d8e772b2ce
kostanzhoglo <jeffreywithers@gmail.com>
2020-06-25 11:59:29 -0400

    Finished. Had to take ties, wins, losses out of function, so they could find variable value

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -6,8 +6,6 @@ user_wins = 0
 user_losses = 0
 user_ties = 0
 
-print(f"{user_wins} Wins, {user_losses} Losses, {user_ties} Ties")
-
 def show_user_choice(user_choice):
     # print(user_choice)
     if user_choice == 'q':
@@ -27,32 +25,27 @@ def show_computer_choice(comp_choice):
     else:
         print('SCISSORS')
 
-def outcomes(user_choice, comp_choice):
-    if user_choice == comp_choice:
-        print("It's a tie!")
-    elif (user_choice == 'r' and comp_choice == 's') or (user_choice == 's' and comp_choice == 'p') or (user_choice == 'p' and comp_choice == 'r'):
-        print("You win!")
-    elif (user_choice == 'r' and comp_choice == 'p') or (user_choice == 's' and comp_choice == 'r') or (user_choice == 'p' and comp_choice == 's'):
-        print("You lose!")
+
+
 
 
 user_choice = ''
 while user_choice != 'q':
     game_options = ['r', 'p', 's']
     comp_choice = random.choice(game_options)
+
+    print(f"{user_wins} Wins, {user_losses} Losses, {user_ties} Ties")
     user_choice = input("Enter your move: (r)ock, (p)aper, (s)cissors or (q)uit:\n")
+    
     show_user_choice(user_choice)
     show_computer_choice(comp_choice)
-    outcomes(user_choice, comp_choice)
 
-
-    # elif user_choice == 'r' and computer_choice == 'r':
-    #     print(user_choice)
-    #     print("ROCK vs. ...")
-    #     print("ROCK\nIt is a tie!")
-    #     user_ties += 1
-    #     print(f"{user_wins} Wins, {user_losses} Losses, {user_ties} Ties")
-    #     continue
-    # elif user_choice == 'r' and computer_choice == 'p':
-    #     print("pause")
-    
+    if user_choice == comp_choice:
+        print("It's a tie!")
+        user_ties = user_ties + 1
+    elif (user_choice == 'r' and comp_choice == 's') or (user_choice == 's' and comp_choice == 'p') or (user_choice == 'p' and comp_choice == 'r'):
+        print("You win!")
+        user_wins = user_wins + 1
+    elif (user_choice == 'r' and comp_choice == 'p') or (user_choice == 's' and comp_choice == 'r') or (user_choice == 'p' and comp_choice == 's'):
+        print("You lose!")
+        user_losses = user_losses + 1
